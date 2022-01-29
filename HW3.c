007217fa5911d8d24edbf18e27b815405f77d1eb
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-29 17:41:47 -0500

    if and else statement

--- HW3.c
+++ HW3.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Main(){
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-
-
+    if (age < 0 || age > 120)
+        printf("Invalid Age");
+    else
+        printf("Thank you, correct input");
+    printf("Done");
+    return 0;
 }

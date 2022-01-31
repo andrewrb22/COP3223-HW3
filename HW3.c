a48d7e4d1329f9261fb3420c878e3c4a8ee79843
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-30 23:44:50 -0500

    created program first test run

--- HW3.c
+++ HW3.c
@@ -1,46 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int Lowest(int a, int b, int c)
 {
-    char choice;
-
-printf("Enter one of this options:\n");
-printf(" '1' to display Africa\n");
-printf(" '2' to display America\n");
-printf(" '3' to display Europa\n");
-printf(" '4' to display Asia\n");
-printf(" '5' to display Austrlia\n");
-printf("Enter your selection:");
-scanf("%c", &choice);
-
-//    if(choice == '1') printf("Africa\n");
-//    else if(choice == '2') printf("America\n");
-//    else if(choice == '3') printf("Europa\n");
-//    else if(choice == '4') printf("Asia\n");
-//    else if(choice == '5') printf("Australia\n");
-//    else printf("Invalid choice");
-
-switch (choice){
-    case '1': printf("\tAfrica\n") ;
-    break;
-
-    case '2': printf("\tAmerica\n");
-    break;
-
-    case '3': printf("\tEuropa\n");
-    break;
-
-    case '4': printf("\tAsia\n");
-    break;
-    
-    case '5': printf("\tAustralia\n");
-    break;
-
-default: printf("Invalid selection");
+    // if  a is lowest 
+    if(a<=b && a<=c)
+        return a;
+    // if b is lowest 
+    else if(b<=a && b<=c)
+        return b;
+    // c is lowest 
+    else 
+        return c;
+}
+int highest(int a, int b, int c)
+{
+    // if a is highest
+    if(a>=b && a>=c)
+        return a;
+    // if b is highest
+    else if(b>=a && b>=c)
+        return b;
+    // c is highest
+    else 
+        return c;
 }
 
-printf("\nThank you");
+int main()
+{
+int a,b,c;
+char choice;
+
+     printf("Enter your three integers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("You have entered the numbers: %d,%d and %d",a,b,c);
+
+ printf("\n\nChoose one of the options:\n");
+    printf("\tA - Print the lowest number entered\n\tB - Print the highest number entered");
+    printf("\n\tC - Print whether the first number entered is an even or odd number");
+    printf("\n\tD - print the division of third number entered by the sum of first two numbers entered");
+    printf("\nMake your selection: ");
+    scanf(" %c",&choice);
+
+  switch (choice)
+    {
+        // if a is the choice
+        case 'a':
+        // call lowest()
+                printf("Lowest number: %d",Lowest(a,b,c));
+                break;
+        case 'b':
+        // if choice is  b 
+        // call highest()
+                printf("highest number: %d",highest(a,b,c));
+                break;
+        case 'c':
+        // if even 
+                if(a%2==0)
+                    printf("%d is even.",a);
+                // if odd 
+                else 
+                    printf("%d is odd.",a);
+                break;
+        case 'd':
+        // if denominator is 0 
+                if((a+b)==0)
+                    printf("Error!!Division by 0 ");
+                else
+                // c divided by sum of a and b 
+                    printf("%d divided by %d is %.2f ",c,(a+b),(float)c/(a+b));
+                break;
+        default: printf("Error!!");
+    };
 
     return 0;
 }

1cdb41262529c25727e96e6dcc1fd19120e5b411
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-30 23:19:12 -0500

    menu option example

--- HW3.c
+++ HW3.c
@@ -3,23 +3,23 @@
 
 int main()
 {
-    int score;
-    char letterGrade = 'x';
+    char choice;
 
-    printf("Enter your score:");
-    scanf("%d", &score);
+printf("Enter one of this options:\n");
+printf(" '1' to display Africa\n");
+printf(" '2' to display America\n");
+printf(" '3' to display Europa\n");
+printf(" '4' to display Asia\n");
+printf(" '5' to display Austrlia\n");
+printf("Enter your selection:");
+scanf("%c", &choice);
 
-    if (score >= 0 && score <= 100)
-    {
-        if (score >= 90) letterGrade = 'A';
-        if (score >= 80 && score< 90) letterGrade = 'B';
-        if (score >= 70 && score< 80) letterGrade = 'C';
-        if (score >= 60 && score< 70) letterGrade = 'D';
-        if(score < 60 ) letterGrade = 'F';
-    }
-
-    if (letterGrade == 'X')  printf("Invalid Score\n");
-    else  printf("You earned the grade of %c", letterGrade);
+   if(choice == '1') printf("Africa\n");
+   else if(choice == '2') printf("America\n");
+   else if(choice == '3') printf("Europa\n");
+   else if(choice == '4') printf("Asia\n");
+   else if(choice == '5') printf("Australia\n");
+   else printf("Invalid choice");
 
     return 0;
 }

b9da781bfdd974e48f16a9b5efe0e27620683baa
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-30 23:24:34 -0500

    switch statement example

--- HW3.c
+++ HW3.c
@@ -14,12 +14,29 @@ printf(" '5' to display Austrlia\n");
 printf("Enter your selection:");
 scanf("%c", &choice);
 
-   if(choice == '1') printf("Africa\n");
-   else if(choice == '2') printf("America\n");
-   else if(choice == '3') printf("Europa\n");
-   else if(choice == '4') printf("Asia\n");
-   else if(choice == '5') printf("Australia\n");
-   else printf("Invalid choice");
+//    if(choice == '1') printf("Africa\n");
+//    else if(choice == '2') printf("America\n");
+//    else if(choice == '3') printf("Europa\n");
+//    else if(choice == '4') printf("Asia\n");
+//    else if(choice == '5') printf("Australia\n");
+//    else printf("Invalid choice");
+
+switch (choice){
+    case '1': printf("\tAfrica\n") ;
+    break;
+
+    case '2': printf("\tAmerica\n");
+    break;
+    case '3': printf("\tEuropa\n");
+    break;
+
+    case '4': printf("\tAsia\n");
+    break;
+    case '5': printf("\tAustralia\n");
+    break;
+
+default: printf("Invalid selection");
+}
 
     return 0;
 }
